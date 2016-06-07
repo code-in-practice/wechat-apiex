0f123782e61cd8cbe63e39e40b5e511e646e017d
zhangshijun <zhangshijun@zcool.com.cn>
2016-06-07 17:14:32 +0800

    auth page get auth url from server

--- config/wechatConfig.js
+++ config/wechatConfig.js
@@ -1,14 +1,15 @@
 module.exports = {
     wechatAppID: 'wx26e0fb0e11a15af4',
     wechatAppsecret: 'd4624c36b6795d1d99dcf0547af5443d',
-    wechatRedirectUri: 'http://huanhuan.rockywu.me/user',
     wechatAuthUrlUserInfo: 'https://open.weixin.qq.com/connect/oauth2/authorize?'
-                    + 'appid=' + this.wechatAppID
-                    + '&redirect_uri=' + this.wechatRedirectUri
+                    + 'appid=' + 'wx26e0fb0e11a15af4'
+                    + '&redirect_uri=' + 'http://huanhuan.rockywu.me/user'
                     + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect',
     wechatAuthUrlBase: 'https://open.weixin.qq.com/connect/oauth2/authorize?'
-                           + 'appid=' + this.wechatAppID
-                           + '&redirect_uri=' + this.wechatRedirectUri
+                           + 'appid=' + 'wx26e0fb0e11a15af4'
+                           + '&redirect_uri=' + 'http://huanhuan.rockywu.me/user'
                            + '&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect',
     token: "rhtsjz"
-}
+};
+
+
