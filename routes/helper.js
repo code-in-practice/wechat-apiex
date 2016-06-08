4f6f35d4b9a8ff105b5debb0e8f23ee3b6ca866b
zhangshijun <zhangshijun@zcool.com.cn>
2016-06-08 09:39:06 +0800

    update jsAcessToken method

--- routes/helper.js
+++ routes/helper.js
@@ -8,15 +8,15 @@ exports.wechatJSAccessToken = function (code, callback) {
     var grant_type = wechatConfig.wechat.grantTypeAuthorizationCode;
     var appid = wechatConfig.wechat.appID;
     var secret = wechatConfig.wechat.appsecret;
-    
-    var options = {
-        url: wechatConfig.wechat.oauth2AccessTokenUrl,
+
+    var url = wechatConfig.wechat.oauth2AccessTokenUrl;
+    var queryStringObj = {
         grant_type: grant_type,
         appid: appid,
         secret: secret
-    }
+    };
 
-    request.get(options, function (error, response, body) {
+    request.get({url: url, qs: queryStringObj}, function (error, response, body) {
         callback(error, response, body);
     })
 };
