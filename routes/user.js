1a8d950d8a5f0df957b82ecb360d61c0deca3b57
zhangshijun <zhangshijun@zcool.com.cn>
2016-06-08 14:51:45 +0800

    user page cookie

--- routes/user.js
+++ routes/user.js
@@ -5,14 +5,28 @@ var wechatConfig = require('./wechatConfig');
 var wechatAuthHelper = require('./wechatAuthHelper');
 
 router.get('/', function (req, res, next) {
-    res.render('user', {});
+    var openid = req.cookies.openid;
+    if(openid){
+        res.render('user', {errMsg: {}, userInfo: {openid: openid}});
+    }else {
+        res.redirect('/user/auth');
+    }
+
 });
 
 
 router.get('/auth', function (req, res, next) {
-    var wechatAuthUrlUserInfo = wechatConfig.wechatAuthUrl(wechatConfig.wechat.redirectUri, wechatConfig.wechat.scopeUserInfo);
-    var wechatAuthUrlBase = wechatConfig.wechatAuthUrl(wechatConfig.wechat.redirectUri, wechatConfig.wechat.scopeBase);
-    res.render('auth', {wechatAuthUrlUserInfo: wechatAuthUrlUserInfo, wechatAuthUrlBase:wechatAuthUrlBase});
+
+    var openid = req.cookies.openid;
+    if(openid){
+        res.redirect('/user');
+    }else {
+        var wechatAuthUrlUserInfo = wechatConfig.wechatAuthUrl(wechatConfig.wechat.redirectUri, wechatConfig.wechat.scopeUserInfo);
+        var wechatAuthUrlBase = wechatConfig.wechatAuthUrl(wechatConfig.wechat.redirectUri, wechatConfig.wechat.scopeBase);
+        res.render('auth', {wechatAuthUrlUserInfo: wechatAuthUrlUserInfo, wechatAuthUrlBase:wechatAuthUrlBase});
+    }
+    
+    
 });
 
 router.get('/auth/callback', function (req, res, next) {
@@ -23,7 +37,10 @@ router.get('/auth/callback', function (req, res, next) {
             res.render('user', {errMsg: body, userInfo: {}});
         }else {
             wechatAuthHelper.jsUserInfo(body.access_token, body.openid, 'zh_CN', function (error, response, body) {
-                res.render('user', {errMsg: {}, userInfo: body});
+                //res.render('user', {errMsg: {}, userInfo: body});
+                var minute = 60 * 1000;
+                req.cookie('openid', body.openid, {maxAge: minute});
+                res.redirect('/user');
             })
         }
         
